cc3ae53e16df36008d6ae0db1afec696d3a66b88
jingwei <qjwwwy@163.com>
2019-12-15 09:37:14 +0800

    initial commit

---
+++ src/main/java/org/my/springboot/qc/qcserver/QcServerApplication.java
@@ -0,0 +1,13 @@
+package org.my.springboot.qc.qcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QcServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/my/springboot/qc/qcserver/QcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.my.springboot.qc.qcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
